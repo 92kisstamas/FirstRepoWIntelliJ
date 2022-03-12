5e749c35d140a5925a58897761a203e96e0ef829
ButcherBence <bence.butcher@gmail.com>
2022-03-12 15:15:58 +0100

    1. feladat vegleges

--- src/hu/progmatic/Main.java
+++ src/hu/progmatic/Main.java
@@ -5,16 +5,21 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+
+        // 1. feladat
+
         System.out.println("Hi!");
         System.out.println("Adj meg egy évet!");
         Scanner scanner = new Scanner(System.in);
         //n > 1582 ; n % 4 == 0 ; n % 100 == 0 && n % 400 == 0;
         int ev = scanner.nextInt();
         scanner.nextLine();
-        boolean szokoev = ev > 1582 && ev % 4 == 0 && ev % 400 ==0;
-        if (szokoev){
+        boolean szokoev = ev > 1582 && ev % 4 == 0;
+        if (szokoev && ev % 100 !=0){
             System.out.println("Az általad megadott év: " + ev + " szökőév.");
-        }else {
+        }else if (ev % 100 == 0 && ev % 400 == 0){
+            System.out.println("Az általad megadott év: " + ev + " szökőév.");
+        }else{
             System.out.println("Az általad megadott év: " + ev + " nem szökőév.");
         }
     }
