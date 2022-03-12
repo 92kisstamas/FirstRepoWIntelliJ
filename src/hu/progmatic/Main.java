1752961b44bb784d6af664e01cc789d0cc1d6bcd
ButcherBence <bence.butcher@gmail.com>
2022-03-12 14:57:14 +0100

    Elso update

--- src/hu/progmatic/Main.java
+++ src/hu/progmatic/Main.java
@@ -1,9 +1,13 @@
 package hu.progmatic;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hi!");
-        //n > 1582 ; n % 4 = 0 ; n % 100 && n % 4;
+        Scanner scanner = new Scanner(System.in);
+        //n > 1582 ; n % 4 == 0 ; n % 100 == 0 && n % 400 == 0;
+
     }
 }
