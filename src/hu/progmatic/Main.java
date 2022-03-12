38f006a3cca7b23014a7de862006cd119e0ccae4
ButcherBence <bence.butcher@gmail.com>
2022-03-12 15:09:07 +0100

    booleeanes

--- src/hu/progmatic/Main.java
+++ src/hu/progmatic/Main.java
@@ -6,8 +6,16 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hi!");
+        System.out.println("Adj meg egy évet!");
         Scanner scanner = new Scanner(System.in);
         //n > 1582 ; n % 4 == 0 ; n % 100 == 0 && n % 400 == 0;
-
+        int ev = scanner.nextInt();
+        scanner.nextLine();
+        boolean szokoev = ev > 1582 && ev % 4 == 0 && ev % 400 ==0;
+        if (szokoev){
+            System.out.println("Az általad megadott év: " + ev + " szökőév.");
+        }else {
+            System.out.println("Az általad megadott év: " + ev + " nem szökőév.");
+        }
     }
 }
